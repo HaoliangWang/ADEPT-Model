b30ae799983b6b0890f2fb125114f978d7d2b5a0
HaoliangWang <hlwang1997@gmail.com>
2024-11-26 22:53:09 -0800

    misc

--- dataset/makers/test.py
+++ dataset/makers/test.py
@@ -10,6 +10,7 @@ import pycocotools.mask as mask_util
 import json
 import yaml
 from glob import glob
+from copy import deepcopy
 
 
 def makedir(path):
@@ -22,6 +23,16 @@ def get_mask_area(color, seg_img):
     arr = arr.min(-1).astype('float')
     arr = arr.reshape((arr.shape[-1], arr.shape[-1]))
     return np.asfortranarray(arr.astype(bool))
+
+def blackout_image(depth_map, area):
+    zero_depth_map = np.full(depth_map.shape, 255)
+    zero_depth_map[area] = depth_map[area]
+    return zero_depth_map
+
+def color_image(depth_map, area, color):
+    _depth_map = deepcopy(depth_map)
+    _depth_map[area] = color
+    return _depth_map
 
 def write_serialized(var, file_name):
     """Write json and yaml file"""
@@ -40,13 +51,14 @@ args = parser.parse_args()
 scenario = args.scenario
 print(scenario)
 
-# source_path = '/ccn2/u/rmvenkat/data/testing_physion/regenerate_from_old_commit/test_humans_consolidated/lf_0'
-source_path = '/mnt/fs5/rahul/lf_0/'
-# save_path = '/ccn2/u/haw027/b3d_ipe/train/'
-save_path = '/mnt/fs0/haw027/b3d_ipe/train/'
+source_path = '/ccn2/u/rmvenkat/data/testing_physion/regenerate_from_old_commit/test_humans_consolidated/lf_0'
+# source_path = '/mnt/fs5/rahul/lf_0/'
+save_path = '/ccn2/u/haw027/b3d_ipe/test_composite/images'
+# save_path = '/mnt/fs0/haw027/b3d_ipe/train/'
 
 width = 350
 height = 350
+thr = 45
 
 file = {}
 scenario_path = join(source_path, scenario+'_all_movies')
@@ -63,7 +75,26 @@ for hdf5_file in onlyhdf5:
     hdf5_file_path = join(scenario_path, hdf5_file)
     with h5py.File(hdf5_file_path, "r") as f:
         object_ids = np.array(f['static']['object_ids'])
+        object_segmentation_colors = np.array(f['static']['object_segmentation_colors'])
         model_names = np.array(f['static']['model_names'])
+
+        fixed_joints = []
+        if "base_id" in np.array(f['static']) and "attachment_id" in np.array(f['static']):
+            base_id = np.array(f['static']['base_id'])
+            attachment_id = np.array(f['static']['attachment_id'])
+            use_cap = np.array(f['static']['use_cap'])
+            assert attachment_id.size==1
+            assert base_id.size==1
+            attachment_id = attachment_id.item()
+            base_id = base_id.item()
+            fixed_joints.append(base_id)
+            fixed_joints.append(attachment_id)
+            if use_cap:
+                cap_id = attachment_id+1
+                fixed_joints.append(cap_id)
+        fixed_joint_ids = np.concatenate([np.where(object_ids==fixed_joint)[0] for fixed_joint in fixed_joints], axis=0).tolist() if fixed_joints else []
+        fixed_joint_ids.sort()
+
         distractors = np.array(f['static']['distractors']) if np.array(f['static']['distractors']).size != 0 else None
         occluders = np.array(f['static']['occluders']) if np.array(f['static']['occluders']).size != 0 else None
         distractor_ids = np.concatenate([np.where(model_names==distractor)[0] for distractor in distractors], axis=0).tolist() if distractors else []
@@ -71,10 +102,10 @@ for hdf5_file in onlyhdf5:
         excluded_model_ids = distractor_ids+occluder_ids
         included_model_ids = [idx for idx in range(len(object_ids)) if idx not in excluded_model_ids]
 
-        object_segmentation_colors = np.array(f['static']['object_segmentation_colors'])
+        
         scales = np.array(f['static']['scale'])
         for key in f['frames'].keys():
-            if int(key)>10:
+            if int(key)>thr:
                 continue
             im = np.array(Image.open(io.BytesIO(f['frames'][key]['images']['_img_cam0'][:])))
             im = Image.fromarray(im)
@@ -89,13 +120,39 @@ for hdf5_file in onlyhdf5:
             this_frame['objects'] = []
             for i in included_model_ids:
                 obj_info = {}
-                obj_info['type'] = model_names[i].decode('UTF-8')
                 obj_info['location'] = locations[i].tolist()
                 obj_info['rotation'] = rotations[i].tolist()
-                obj_info['scale'] = scales[i].tolist()
+                if i in fixed_joint_ids:
+                    if fixed_joint_ids.index(i) == 0:
+                        for j, o_id in enumerate(fixed_joint_ids):
+                            this_color = object_segmentation_colors[o_id]
+                            this_area = get_mask_area(im_seg, this_color)
+                            if j == 0:
+                                consistent_color = this_color
+                            else:
+                                im_seg = color_image(im_seg, this_area, consistent_color)
+                        if len(fixed_joint_ids)==3:
+                            obj_info['type'] = "cyn_cyn_cyn"
+                        elif len(fixed_joint_ids)==2:
+                            if "cone" in [model_names[idd].decode("utf-8") for idd in fixed_joint_ids]:
+                                obj_info['type'] = "cyn_cone"
+                            else:
+                                obj_info['type'] = "cyn_cyn"
+                        else:
+                            obj_info['type'] = [model_names[i].decode('UTF-8')]
+                        composite_scale_y = 0
+                        for k in fixed_joint_ids:
+                            composite_scale_y += scales[k][1]
+                        obj_info['scale'] = scales[i].tolist()
+                        obj_info['scale'][1] = composite_scale_y
+                    else:
+                        continue
+                else:
+                    obj_info['type'] = [model_names[i].decode('UTF-8')]
+                    obj_info['scale'] = scales[i].tolist()
                 obj_mask = mask_util.encode(get_mask_area(im_seg, object_segmentation_colors[i]))
                 obj_info['mask'] = {'size': obj_mask['size'], 'counts': obj_mask['counts'].decode('UTF-8')}
                 this_frame['objects'].append(obj_info)
             file[trial_name]['scene'].append(this_frame)
 
-write_serialized(file, join('/'.join(save_path.split('/')[:-2]), f'annotated_ann_{scenario}.json'))
+write_serialized(file, join('/'.join(save_path.split('/')[:-1]), f'annotated_ann_{scenario}.json'))
--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
 CONTENT_FOLDER = "/mnt/fs0/haw027/data/"
 
 TERMS = ["type", "location", "rotation", "scale"]
-TYPES = ["bowl", "cone", "cube", "cylinder", "ramp_with_platform_30", "octahedron", "pentagon", "pipe", "platonic", "pyramid", "sphere", "torus", "triangular_prism"]
+TYPES = ["bowl", "cone", "cube", "cylinder", "ramp_with_platform_30", "octahedron", "pentagon", "pipe", "platonic", "pyramid", "sphere", "torus", "triangular_prism", "cyn_cyn_cyn", "cyn_cone", "cyn_cyn"]
